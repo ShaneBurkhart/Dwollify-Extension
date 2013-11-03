a59b50c3e31d8dde1b9176895785d01b87703eb5
Shane Burkhart <shaneburkhart@gmail.com>
2013-11-03 15:45:05 -0600

    Ryan's turn

--- popup.js
+++ popup.js
@@ -9,7 +9,25 @@ jQuery(function(){
         console.log(request.emails);
         $("#email-list").text('');
         for(var i = 0 ; i < request.emails.length ; i ++)
-          $("#email-list").append('<li>' + request.emails[i] + '</li>');
+          $("#email-list").append([
+            '<li>',
+              '<a href="#" class="email" data-email="', request.emails[i], '">',
+                request.emails[i],
+              '</a>',
+            '</li>'
+          ].join(""));
+        $(".email").unbind();
+        $(".email").bind("click", function(){
+          var email = $(this).data("email");
+          $.ajax({
+              type: "POST",
+              url: "http://dwollify.herokuapp.com/dwollify",
+              data: JSON.stringify({
+                "email" : email,
+                "amount" : "0.01"
+              })
+          });
+        });
       }
     }
 
